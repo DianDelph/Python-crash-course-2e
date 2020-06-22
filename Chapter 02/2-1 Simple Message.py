5e11e3694301b5ced9ef55d24111c1c0334b49f0
DianDelph <da.dnvl@live.com>
2020-06-21 21:16:25 -0500

    Resuelve ejercicios 2-1 y 2-2

---
+++ Chapter 02/2-1 Simple Message.py
@@ -0,0 +1,5 @@
+#asigna un mensaje a una variable y luego imprime ese mensaje
+
+message= "¡Hola a todos!" #asigno el mensaje (valor) a la variable
+
+print(message) #imprimo el VALOR de la variable "message"
---
+++ Chapter 02/2-2 Simple Messages.py
@@ -0,0 +1,9 @@
+#asigna un mensaje a una variable, la imprime, asigna otro valor y vuelve a imprimir la variable
+
+message = "¡Hola a todos!" #asigna el mensaje (Valor) a la variable
+
+print(message) #imprime el valor (mensaje) de la variable
+
+message = "¡¿Qué onda?!" #cambia el mensaje (valor) por uno nuevo
+
+print (message) #imprimo la misma variable, ahora con el nuevo valor (mensaje)
