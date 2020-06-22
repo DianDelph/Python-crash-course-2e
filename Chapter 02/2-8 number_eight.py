604228464ba1070969b040be8aeb1ad5d506156a
DianDelph <da.dnvl@live.com>
2020-06-22 11:23:55 -0500

    resuelve ejercicios 2-8 y 2-9

---
+++ Chapter 02/2-8 number_eight.py
@@ -0,0 +1,7 @@
+# escribe cuatro operaciones ( + - / * ) que resulten en 8
+
+print(4+4)
+print(10-2)
+print(24/3)
+print(1*8)
+
---
+++ Chapter 02/2-9 favorite_number.py
@@ -0,0 +1,7 @@
+#usa una variable para representar el número favorito. Luego usa esa variable para crear un mensaje que revele el número, imprime el mensaje
+
+fav_number = 25
+
+message = f"Mi numero favorito es {fav_number}" #usando f-strings guardo el valor de la variable fav_number dentro del mensaje
+
+print(message)
