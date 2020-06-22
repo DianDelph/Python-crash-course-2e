a606b3acb0106830e50529a855503eae4b42e782
DianDelph <da.dnvl@live.com>
2020-06-22 11:51:39 -0500

    Resuelve ejericcios 3-1 y 3-2

---
+++ Chapter 03/3-1 names.py
@@ -0,0 +1,7 @@
+# guarda nombres en una lista, luego los imprime uno por uno
+
+names = ['Ada', 'Mary', 'Ursula'] #guarda los nombres en una lista
+
+print(names[0]) #en programación se empiezan desde 0 los índices
+print(names[1])
+print(names[2])
---
+++ Chapter 03/3-2 greetings.py
@@ -0,0 +1,15 @@
+#usa la lista de 3-1 e imprime un mensaje personalizado a cada miembro.
+
+names = ['ada', 'mary', 'ursula'] 
+
+message = f'¡Hola {names[0].title()}!' #usa f-strings para guardar el mensaje personalizado. Comienza con el primer elemento y le da formato de título
+
+print(message) 
+
+message = f'¡Hola {names[1].title()}!'
+
+print(message)
+
+message = f'¡Hola {names[2].title()}!'
+
+print(message)
